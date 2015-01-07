f3f2c66e45ede65cecd856e99255e4c029717605
Patrick Lerner <patricklerner@me.com>
2015-01-07 16:17:05 +0100

    Fix in the forumula

--- src/main/java/de/tudarmstadt/awesome/erclaerung/feature/WSoundUUDominanceDFE.java
+++ src/main/java/de/tudarmstadt/awesome/erclaerung/feature/WSoundUUDominanceDFE.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.tc.api.exception.TextClassificationException;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.DocumentFeatureExtractor;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
@@ -17,12 +19,12 @@ public class WSoundUUDominanceDFE extends FeatureExtractorResource_ImplBase impl
 	public List<Feature> extract(JCas jcas) throws TextClassificationException {
 		String documentText = jcas.getDocumentText().toLowerCase();
 
-		double w_sound_as_w = StringUtils.countMatches(documentText, "w") * 0.5d;
-		double w_sound_as_uu = StringUtils.countMatches(documentText, "uu");
+		double count_uu = StringUtils.countMatches(documentText, "uu");
+		double count_tokens = JCasUtil.select(jcas, Token.class).size();
 
-		double res = 0;
-		if (w_sound_as_w > 0 && w_sound_as_uu > 0)
-			res = w_sound_as_uu / w_sound_as_w;
+		double res = 1000 * count_uu / count_tokens;
+
+		// System.out.println("[UU] " + res);
 
 		List<Feature> featList = new ArrayList<Feature>();
 		featList.add(new Feature(FN_UU_DOMINANCE, res));
