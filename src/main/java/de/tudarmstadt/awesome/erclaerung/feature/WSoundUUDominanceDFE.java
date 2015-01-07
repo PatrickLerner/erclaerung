7852f38b97cfb96277a8bac5df273392cfc9da19
Patrick Lerner <patricklerner@me.com>
2015-01-07 15:31:23 +0100

    Slight adjustment to the w-sound analyser

--- src/main/java/de/tudarmstadt/awesome/erclaerung/feature/WSoundUUDominanceDFE.java
+++ src/main/java/de/tudarmstadt/awesome/erclaerung/feature/WSoundUUDominanceDFE.java
@@ -17,7 +17,7 @@ public class WSoundUUDominanceDFE extends FeatureExtractorResource_ImplBase impl
 	public List<Feature> extract(JCas jcas) throws TextClassificationException {
 		String documentText = jcas.getDocumentText().toLowerCase();
 
-		double w_sound_as_w = StringUtils.countMatches(documentText, "w");
+		double w_sound_as_w = StringUtils.countMatches(documentText, "w") * 0.5d;
 		double w_sound_as_uu = StringUtils.countMatches(documentText, "uu");
 
 		List<Feature> featList = new ArrayList<Feature>();
--- src/main/java/de/tudarmstadt/awesome/erclaerung/readers/UnlabeledTextReader.java
+++ src/main/java/de/tudarmstadt/awesome/erclaerung/readers/UnlabeledTextReader.java
@@ -7,15 +7,11 @@ import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.jcas.JCas;
 
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
 import de.tudarmstadt.ukp.dkpro.tc.api.io.TCReaderSingleLabel;
 import de.tudarmstadt.ukp.dkpro.tc.api.type.TextClassificationOutcome;
 import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
 
-/**
- * Reads plain text tweets, labels each tweet as sentence, and assigns no label to them.
- */
 public class UnlabeledTextReader extends TextReader implements TCReaderSingleLabel {
 	@Override
 	public void getNext(CAS aCAS) throws IOException, CollectionException {
@@ -24,11 +20,6 @@ public class UnlabeledTextReader extends TextReader implements TCReaderSingleLab
 		JCas jcas;
 		try {
 			jcas = aCAS.getJCas();
-			// consider a tweet to be a sentence
-			Sentence sentenceAnno = new Sentence(jcas);
-			sentenceAnno.setBegin(0);
-			sentenceAnno.setEnd(jcas.getDocumentText().length());
-			sentenceAnno.addToIndexes();
 		}
 		catch (CASException e) {
 			throw new CollectionException();
