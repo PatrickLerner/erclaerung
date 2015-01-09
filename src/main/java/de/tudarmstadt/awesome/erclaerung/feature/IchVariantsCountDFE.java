0444da1c3f5f5a1cd1f59abe0d2a419295cb9f4c
Patrick Lerner <patricklerner@me.com>
2015-01-09 04:01:58 +0100

    Some clean up

--- src/main/java/de/tudarmstadt/awesome/erclaerung/feature/IchVariantsCountDFE.java
+++ src/main/java/de/tudarmstadt/awesome/erclaerung/feature/IchVariantsCountDFE.java
@@ -16,6 +16,13 @@ import de.tudarmstadt.ukp.dkpro.tc.api.features.DocumentFeatureExtractor;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
 
+/**
+ * This engine analyses how common various ways to say the word modern German word "ich" exist in any source text. Some
+ * variants "ik" are more common in northern German texts and softer variants such as "ich" are more common in central
+ * and southern dialects.
+ * 
+ * @author patrick
+ */
 public class IchVariantsCountDFE extends FeatureExtractorResource_ImplBase implements DocumentFeatureExtractor {
 	public static final String FN_ICH_VARIANT_PREFIX = "LetterRatio_";
 	public final String[] ICH_VARIANTS_VALUES = { "i", "ig", "ich", "ik", "ih", "ek", "isch", "ish" };
@@ -24,17 +31,21 @@ public class IchVariantsCountDFE extends FeatureExtractorResource_ImplBase imple
 	public List<Feature> extract(JCas jcas) throws TextClassificationException {
 		List<String> tokens = JCasUtil.toText(JCasUtil.select(jcas, Token.class));
 
+		// initialize the count for all variants with zero
 		HashMap<String, Double> variantCount = new HashMap<String, Double>();
 		for (String v : ICH_VARIANTS)
 			variantCount.put(v, 0d);
 
+		// go through all of a text's tokens and count the various variants
 		for (String token : tokens)
 			if (ICH_VARIANTS.contains(token.toLowerCase()))
 				variantCount.put(token.toLowerCase(), variantCount.get(token.toLowerCase()) + 1);
 
+		// generate a feature list
 		List<Feature> featList = new ArrayList<Feature>();
-
 		for (String variant : ICH_VARIANTS) {
+			// for each variant add a feature, but normalize it over text length to make it valid
+			// for comparisons
 			double count = variantCount.get(variant) * 1000 / tokens.size();
 			featList.add(new Feature(FN_ICH_VARIANT_PREFIX + variant, count));
 			// System.out.println("[ICH-VAR] [" + variant + "] " + count);
--- src/main/java/de/tudarmstadt/awesome/erclaerung/feature/LetterDistributionDFE.java
+++ src/main/java/de/tudarmstadt/awesome/erclaerung/feature/LetterDistributionDFE.java
@@ -11,17 +11,28 @@ import de.tudarmstadt.ukp.dkpro.tc.api.features.DocumentFeatureExtractor;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
 
+/**
+ * The following engine analyses how often specific letters are used in a text.
+ * 
+ * @author patrick
+ */
 public class LetterDistributionDFE extends FeatureExtractorResource_ImplBase implements DocumentFeatureExtractor {
 	public static final String FN_LETTER_RATIO_PREFIX = "LetterRatio";
+
+	/**
+	 * The letters we are actually looking at, discounting any 'special characters' or other unknown ones
+	 */
 	public static final String LETTERS = "abcdefghijklmnopqrstuvwxyzßäöü";
 
 	public List<Feature> extract(JCas jcas) throws TextClassificationException {
 		String documentText = jcas.getDocumentText().toLowerCase();
 		HashMap<Character, Double> letterCount = new HashMap<Character, Double>();
 
+		// initialize hashmap
 		for (char l : LETTERS.toCharArray())
 			letterCount.put(l, 0d);
 
+		// go through all letters to see what we actuall have
 		double totalLetterCount = 0;
 		for (char l : documentText.toCharArray()) {
 			if (letterCount.containsKey(l)) {
@@ -30,8 +41,8 @@ public class LetterDistributionDFE extends FeatureExtractorResource_ImplBase imp
 			}
 		}
 
+		// create one feature for each letter, normalizing over total count
 		List<Feature> featList = new ArrayList<Feature>();
-
 		for (char letter : letterCount.keySet()) {
 			String letter_str = new String(new char[] { letter });
 			double countLetter = letterCount.get(letter);
