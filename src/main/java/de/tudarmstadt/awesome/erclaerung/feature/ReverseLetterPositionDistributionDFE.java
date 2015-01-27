b8f470904687f18006080817c62a6d640a88459c
Manuel Spari <manuel.spari@gmail.com>
2015-01-27 22:55:10 +0100

    sync

--- src/main/java/de/tudarmstadt/awesome/erclaerung/feature/ReverseLetterPositionDistributionDFE.java
+++ src/main/java/de/tudarmstadt/awesome/erclaerung/feature/ReverseLetterPositionDistributionDFE.java
@@ -9,24 +9,31 @@ import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-import de.tudarmstadt.ukp.dkpro.tc.api.exception.TextClassificationException;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
 
-public class LetterPositionDistributionDFE extends FeatureExtractorResource_ImplBase {
-	public static final String FN_LETTER_POSITION_PREFIX = "Letter_Position";
+public class ReverseLetterPositionDistributionDFE extends FeatureExtractorResource_ImplBase {
+	public static final String FN_REVERSE_LETTER_POSITION_PREFIX = "Letter_Position";
 	public static final String LETTERS = "abcdefghijklmnopqrstuvwxyzßäöü";
 	public static final int[] POSITIONS = { 0, 1 };
 
-	/*
-	 * private boolean positionsContains(int i) { for (int j : POSITIONS) { if (i == j) return true; } return false; }
-	 * 
-	 * 
-	 * private boolean lettersContains(Character c) { for (char d : LETTERS.toCharArray()) { if (c == d) return true; }
-	 * return false; }
-	 */
+	private boolean positionsContains(int i) {
+		for (int j : POSITIONS) {
+			if (i == j)
+				return true;
+		}
+		return false;
+	}
 
-	public List<Feature> extract(JCas jcas) throws TextClassificationException {
+	private boolean lettersContains(Character c) {
+		for (char d : LETTERS.toCharArray()) {
+			if (c == d)
+				return true;
+		}
+		return false;
+	}
+
+	public List<Feature> extract(JCas jcas) {
 		// Initialize
 		HashMap<String, Double> lettersAndPositions = new HashMap<String, Double>();
 		for (Character c : LETTERS.toCharArray()) {
@@ -40,22 +47,15 @@ public class LetterPositionDistributionDFE extends FeatureExtractorResource_Impl
 		for (String token : tokens) {
 			Character[] letterArray = ArrayUtils.toObject(token.toCharArray());
 			for (int i = 0; i < letterArray.length; i++) {
-				for (int j : POSITIONS) {
-					if (i == j) {
-						char k = letterArray[i];
-						for (char d : LETTERS.toCharArray()) {
-							if (k == d)
-								count++;
-						}
-					}
-				}
+				if (positionsContains(i) && lettersContains(letterArray[letterArray.length - 1 - i]))
+					count++;
 				lettersAndPositions.put(letterArray[i].toString() + i,
-				                lettersAndPositions.get(letterArray[i].toString() + i));
+				                lettersAndPositions.get(letterArray[letterArray.length - 1 - i].toString() + i));
 			}
 		}
 		List<Feature> featList = new ArrayList<Feature>();
 		for (String key : lettersAndPositions.keySet()) {
-			featList.add(new Feature(FN_LETTER_POSITION_PREFIX + key, lettersAndPositions.get(key) / count));
+			featList.add(new Feature(FN_REVERSE_LETTER_POSITION_PREFIX + key, lettersAndPositions.get(key) / count));
 		}
 		return featList;
 	}
